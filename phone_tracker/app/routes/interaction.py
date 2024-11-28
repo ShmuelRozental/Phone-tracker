ae0f3b3e07d56c9bdf5287877d3ded5355ed5feb
ShmuelRozental <rozentalshmuel@gmail.com>
2024-11-28 17:31:48 +0200

    add the rest of routes

--- phone_tracker/app/routes/interaction.py
+++ phone_tracker/app/routes/interaction.py
@@ -33,3 +33,50 @@ def get_interaction():
             return jsonify({"error": "Devices list is incomplete"}), 400
     else:
         return jsonify({"error": "Invalid data format, expected a list"}), 400
+
+
+
+
+@phone_blueprint.route("/api/bluetooth_connections", methods=['GET'])
+def get_bluetooth_connections():
+    device_repo = DeviceRepository(get_driver())
+    bluetooth_connections = device_repo.count_bluetooth_connections()
+
+    return jsonify({'result': bluetooth_connections}), 200
+
+
+@phone_blueprint.route("/api/strong_signal_devices", methods=['GET'])
+def find_stronger_devices():
+    data = request.args.get('signal_strength_dbm', -60)
+    device_repo = DeviceRepository(get_driver())
+    strong_signal_devices = device_repo.find_devices_with_signal_strength(int(data))
+
+    return jsonify({'result': strong_signal_devices}), 200
+
+
+@phone_blueprint.route("/api/device_connections", methods=['GET'])
+def count_connected_devices():
+    device_id = request.args.get('device_id')
+    device_repo = DeviceRepository(get_driver())
+    device_connections = device_repo.count_device_connections(device_id)
+
+    return jsonify({'result': device_connections}), 200
+
+
+@phone_blueprint.route("/api/direct_connection", methods=['GET'])
+def check_direct_connection():
+    from_device_id = request.args.get('from_device_id')
+    to_device_id = request.args.get('to_device_id')
+    device_repo = DeviceRepository(get_driver())
+    is_direct_connection = device_repo.is_device_direct_connection(from_device_id, to_device_id)
+
+    return jsonify({'result': is_direct_connection}), 200
+
+
+@phone_blueprint.route("/api/most_recent_interaction", methods=['GET'])
+def get_most_recent_interaction():
+    device_id = request.args.get('device_id')
+    device_repo = DeviceRepository(get_driver())
+    most_recent_interaction = device_repo.find_most_recent_interaction(device_id)
+
+    return jsonify({'result': most_recent_interaction}), 200
