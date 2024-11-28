8e289cc597e5ce6acd9bb6cf6e95dcd1b9743a5e
ShmuelRozental <rozentalshmuel@gmail.com>
2024-11-28 17:36:06 +0200

    final configureate

--- phone_tracker/app/main.py
+++ phone_tracker/app/main.py
@@ -1,18 +1,18 @@
+import os
 from flask import Flask
-from phone_tracker.app.neo4j import init_driver
+from neo4j_driver import init_driver
 from routes.interaction import phone_blueprint
-from neo4j import GraphDatabase
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+
 app.register_blueprint(phone_blueprint)
 
-
 if __name__ == '__main__':
-       with app.app_context():
+    with app.app_context():
         init_driver(
-            app.config.get('NEO4J_URI'),
-            app.config.get('NEO4J_USERNAME'),
-            app.config.get('NEO4J_PASSWORD'),
         )
-        app.run(host='0.0.0.0', port=5000, debug=True)  
+        app.run(host='0.0.0.0', port=5000, debug=True)
